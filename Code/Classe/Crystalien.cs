f53a02fe19465f8bfd19df3df9e9594f633cd441
Guillaume Paoli <wdgaster.gtp@gmail.com>
2022-12-12 09:32:31 -0500

    first commit
    4h

---
+++ Code/Classe/Crystalien.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Final
+{
+    internal class Crystalien : Utilisateur
+    {
+        //Attribut
+        private string habitant;
+        public List<Mission> lstMission;
+
+        //Get-Set
+        public string Habitant
+        {
+            get { return habitant; }
+            set {
+                    if (value == "Langitia")
+                        habitant = value;
+                    else if (value == "Lurminia")
+                        habitant = value;
+                    else if (value == "Technoria")
+                        habitant = value;
+                    else if (value == "Cartage")
+                        habitant = value;
+                    else if (value == "Vierde")
+                        habitant = value;
+            }
+        }
+
+        //Construct
+        public Crystalien(
+            string p_matricule = "",
+            string p_nom = "",
+            int p_droit = 0,
+            string p_habitant = ""
+            ):base(p_matricule,p_nom,p_droit)
+    
+        //Methode
+        public override void Sauvegarde()
+        {
+            
+        }
+    }
+}
---
+++ Code/Classe/Utilisateur.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Final
+{
+    internal abstract class Utilisateur
+    {
+        //Attribut
+        protected string matricule;
+        protected string nom;
+        protected int droit;
+
+        //Get-Set
+        public string Matricule
+        {
+            get { return matricule; }
+            set { matricule = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public int Droit
+        {
+            get { return droit; }
+            set { if (value >= 0 && value <= 3) { droit = value; } }
+        }
+
+        //Construct
+        public Utilisateur(
+            string p_martricule = "",
+            string p_nom = "",
+            int p_droit = 0)
+        { 
+            this.matricule = p_martricule;
+            this.nom = p_nom;
+            this.droit = p_droit;
+        }
+
+        //Methode
+        public abstract void Sauvegarde();
+    }
+}
